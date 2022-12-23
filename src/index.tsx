b1d38de663cdb5c644fc8a94e5e4210a06fdddb2
shahin1290 <s.patowary@yahoo.com>
2022-12-23 21:16:11 +0100

    add storybook and stories

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// import { Legend } from "./Legend/Legend";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+// root.render(<Legend />);
