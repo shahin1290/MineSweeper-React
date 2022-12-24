418ca606e7907365054d631e4d2acdcb774aa5c3
shahin1290 <s.patowary@yahoo.com>
2022-12-24 08:44:29 +0100

    adds scoreboard component with stories, testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-// import { Legend } from "./Legend/Legend";
+import { Scoreboard } from "./components/Scoreboard";
+
+import { Top } from "./components/Top";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
 
-// root.render(<Legend />);
+root.render(
+  <>
+    <Top feature="Flag" firstAction="ctrl" secondAction="click">
+      Minesweeper
+    </Top>
+    <Scoreboard
+      time="000"
+      levels={["beginner", "intermediate", "expert"]}
+      mines="010"
+      onReset={() => null}
+    />
+  </>
+);
